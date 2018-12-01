bd215c0ec147aad8126214ce868748f33ce354ad
whimo <me@whimo.me>
2018-12-01 09:24:00 +0300

    Improved apply_patchwise

--- utils.py
+++ utils.py
@@ -1,21 +1,37 @@
+def apply_to_patch(img, fn, coord, patch_size):
+    '''
+    Applies given function to a square patch of an image
+    '''
+    y, x = coord
+
+    patch = img[y:y + patch_size,
+                x:x + patch_size]
+    patch = fn(patch)
+
+    img[y:y + patch_size,
+        x:x + patch_size] = patch
+
+    return img
+
+
 def apply_patchwise(img, fn, patch_size=256):
     '''
-    Function that takes an image and applies given function to every square patch of it
+    Function that splits image into square patches and apples given function to every of them
     '''
     height, width, _ = img.shape
 
     offset_height, offset_width = 0, 0
 
     while offset_height <= height - patch_size:
-        while offset_width <= width - patch_size:
-            patch = img[offset_height:offset_height + patch_size,
-                        offset_width:offset_width + patch_size]
-            patch = fn(patch)
-
-            img[offset_height:offset_height + patch_size,
-                offset_width:offset_width + patch_size] = patch
-
+        while offset_width < width - patch_size:
+            apply_to_patch(img, fn, (offset_height, offset_width), patch_size)
             offset_width += patch_size
+        apply_to_patch(img, fn, (offset_height, width - patch_size), patch_size)
 
         offset_height += patch_size
         offset_width = 0
+
+        if offset_height > height - patch_size and offset_height < height:
+            offset_height = height - patch_size
+
+    return img
